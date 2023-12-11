d46659be5bea848154c5314e052d08d8b02f24f7
Daniel Harris <danzo2303@gmail.com>
2023-12-11 18:13:23 +0000

    Asynch.

--- Program.cs
+++ Program.cs
@@ -1,20 +1,57 @@
 ﻿using Raylib_cs;
+using System.Numerics;
 
-namespace HelloWorld;
+public class InputMouse {
+    public static int Main() {
+        Raylib.InitWindow(16*100, 9*100, "GraphRogue");
+        Raylib.SetWindowState(ConfigFlags.FLAG_WINDOW_RESIZABLE);
+        Raylib.MaximizeWindow();
 
-class Program {
-    public static void Main() {
-        Raylib.InitWindow(800, 480, "Hello World");
-
-        while (!Raylib.WindowShouldClose()) {
-            Raylib.BeginDrawing();
-            Raylib.ClearBackground(Color.WHITE);
-
-            Raylib.DrawText("Hello, world!", 12, 12, 20, Color.BLACK);
-
-            Raylib.EndDrawing();
-        }
+        new Thread(ComputeLoop).Start();
+        UILoop();
 
         Raylib.CloseWindow();
+        return 0;
+    }
+
+    //Non-time-critical work such as step simulation
+    static void ComputeLoop() {
+        int i = 0;
+        while (true) {
+            Console.WriteLine(i++);
+            for (int j = 0; j<Math.Pow(10,8); j++) {}
+        }
+    }
+
+    //UX-critical work such as responding to input
+    static void UILoop() {
+        Vector2 ballPosition = new(-100.0f, -100.0f);
+        Color ballColor = Color.DARKBLUE;
+
+        Raylib.SetTargetFPS(60);
+        while (!Raylib.WindowShouldClose()) {
+
+            ballPosition = Raylib.GetMousePosition();
+
+            if (Raylib.IsMouseButtonPressed(MouseButton.MOUSE_LEFT_BUTTON)) {
+                ballColor = Color.MAROON;
+            } else if (Raylib.IsMouseButtonPressed(MouseButton.MOUSE_MIDDLE_BUTTON)) {
+                ballColor = Color.LIME;
+            } else if (Raylib.IsMouseButtonPressed(MouseButton.MOUSE_RIGHT_BUTTON)) {
+                ballColor = Color.DARKBLUE;
+            }
+
+            Raylib.BeginDrawing();
+
+
+            Raylib.ClearBackground(Color.RAYWHITE);
+
+            Raylib.DrawCircleV(ballPosition, 40, ballColor);
+
+            Raylib.DrawText("move ball with mouse and click mouse button to change color", 10, 10, 20, Color.DARKGRAY);
+            Raylib.EndDrawing(); //From my testing it seems EndDrawing() waits to cap the framerate as set by SetTargetFPS(60)
+
+
+        }
     }
 }
