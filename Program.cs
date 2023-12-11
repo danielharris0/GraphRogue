a0cea653112c6002242249d3b94248e9e0f2849d
Daniel Harris <danzo2303@gmail.com>
2023-12-11 18:20:06 +0000

    Task Paradigm

--- Program.cs
+++ Program.cs
@@ -7,24 +7,6 @@ public class InputMouse {
         Raylib.SetWindowState(ConfigFlags.FLAG_WINDOW_RESIZABLE);
         Raylib.MaximizeWindow();
 
-        new Thread(ComputeLoop).Start();
-        UILoop();
-
-        Raylib.CloseWindow();
-        return 0;
-    }
-
-    //Non-time-critical work such as step simulation
-    static void ComputeLoop() {
-        int i = 0;
-        while (true) {
-            Console.WriteLine(i++);
-            for (int j = 0; j<Math.Pow(10,8); j++) {}
-        }
-    }
-
-    //UX-critical work such as responding to input
-    static void UILoop() {
         Vector2 ballPosition = new(-100.0f, -100.0f);
         Color ballColor = Color.DARKBLUE;
 
@@ -34,6 +16,7 @@ public class InputMouse {
             ballPosition = Raylib.GetMousePosition();
 
             if (Raylib.IsMouseButtonPressed(MouseButton.MOUSE_LEFT_BUTTON)) {
+                new Task(LongTask).Start();
                 ballColor = Color.MAROON;
             } else if (Raylib.IsMouseButtonPressed(MouseButton.MOUSE_MIDDLE_BUTTON)) {
                 ballColor = Color.LIME;
@@ -50,8 +33,18 @@ public class InputMouse {
 
             Raylib.DrawText("move ball with mouse and click mouse button to change color", 10, 10, 20, Color.DARKGRAY);
             Raylib.EndDrawing(); //From my testing it seems EndDrawing() waits to cap the framerate as set by SetTargetFPS(60)
+        }
 
+        Raylib.CloseWindow();
+        return 0;
+    }
 
+    static void LongTask() {
+        for (int i=0; i<10; i++) {
+            Console.WriteLine(i);
+            for (int j = 0; j<Math.Pow(10,7); j++) {}
         }
     }
+
+
 }
